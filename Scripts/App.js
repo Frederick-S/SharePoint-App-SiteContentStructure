ea64e6e525ab51e8641d76be74899bd4d4b1fcd9
Frederick-S <mao_xiaodan@hotmail.com>
2015-04-01 15:50:29 +0800

    Add spinner

--- Scripts/App.js
+++ Scripts/App.js
@@ -1,1 +1,24 @@
-﻿
+﻿(function ($, SP) {
+    var Node = function () {
+        this.title = '';
+        this.serverRelativeUrl = '';
+        this.absoluteUrl = '';
+        this.type = '';
+        this.iconUrl = '';
+        this.children = [];
+    };
+
+    function getQueryStringParameters() {
+        var params = document.URL.split("?")[1].split("&");
+        var obj = {};
+
+        for (var i = 0; i < params.length; i = i + 1) {
+            var singleParam = params[i].split("=");
+            obj[singleParam[0]] = decodeURIComponent(singleParam[1]);
+        }
+
+        return obj;
+    }
+
+
+})(jQuery, SP);
